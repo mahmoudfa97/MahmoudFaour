6c04b6eeeda97c9e75b1a62f9df2939bd17e1c09
Mahmoud Faour <leedor97@gmail.com>
2024-04-26 12:21:23 +0300

    Update Skills.tsx

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -3,16 +3,16 @@ import ProgressBar from "./progressBar";
 
 const Skills = () => {
   let firstRowSkills = [
-    { name: "JavaScript", val: "100" },
-    { name: "React", val: "100" },
-    { name: "TypeScript", val: "100" },
-    { name: "NodeJS", val: "95" },
+    { name: "JavaScript", val: "90" },
+    { name: "React", val: "90" },
+    { name: "TypeScript", val: "90" },
+    { name: "NodeJS", val: "90" },
   ];
   let secondRowSkills = [
-    { name: "Redux", val: "100" },
-    { name: "Hooks", val: "100" },
-    { name: "Angular", val: "75" },
-    { name: "Webpack", val: "70" },
+    { name: "Redux", val: "90" },
+    { name: "Hooks", val: "90" },
+    { name: "Angular", val: "80" },
+    { name: "Webpack", val: "90" },
   ];
   return (
     <section id="skills" className="skills section-bg">
